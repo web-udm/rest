d83f1177728ae0d88934f311a19be3546ab035d4
Ildar Mosnabeev <web.udm@gmail.com>
2021-07-05 01:21:24 +0400

    feature-48 Каркас для билдера

--- src/Builders/ResponseBuilder.php
+++ src/Builders/ResponseBuilder.php
@@ -3,6 +3,7 @@
 namespace App\Builders;
 
 use App\Entities\EntityInterface;
+use App\Responses\BaseResponse;
 
 class ResponseBuilder
 {
@@ -11,28 +12,51 @@ class ResponseBuilder
         string $message,
         string $type = 'json',
         array $headers = []
-    ): BaseResponse {
+    ) {
+        $content = $this->serialize([
+            'code' => $code,
+            'message' => $message
+        ], $type);
 
+        $headers = array_merge($headers, $this->getContentType($type));
+
+        return new BaseResponse($content, $code, $headers);
     }
 
     public function createEntityResponse(
-        int $code,
-        string $message,
         EntityInterface $entity,
+        int $code = 200,
+        string $message = "User successfully fetched",
         string $type = 'json',
-        array $headers = []
+        array $headers = ['Content-Type' => 'application/json']
     ) {
 
     }
 
     public function createEntitiesResponse(
-        int $code,
-        string $message,
         EntityInterface $entities,
         array $metadata,
+        int $code = 200,
+        string $message = "Users successfully fetched",
         string $type = 'json',
-        array $headers = []
+        array $headers = ['Content-Type' => 'application/json']
     ) {
+
+    }
+
+    private function serialize(array $data, string $type)
+    {
+        $serializerName = "\App\Serializers\{$type}Serializer";
+
+        if (class_exists($serializerName)) {
+            return $serializerName->serialize($data);
+        }
+
+        throw new \Exception("Для типа $type не найден подходящий сериалайзер");
+    }
+
+    private function getContentType(string $type)
+    {
 
     }
 }
