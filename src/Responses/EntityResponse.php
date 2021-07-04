d04d202a629f9a5cbfc37582ca6e421524aa7d2c
Ildar Mosnabeev <web.udm@gmail.com>
2021-07-05 01:21:40 +0400

    feature-48 Очистил Responses

--- src/Responses/EntityResponse.php
+++ src/Responses/EntityResponse.php
@@ -2,19 +2,6 @@
 
 namespace App\Responses;
 
-use App\Entities\EntityInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
-class EntityResponse extends JsonResponse
+class EntityResponse extends Response
 {
-    public function __construct(EntityInterface $entity, string $status, string $message, array $headers = [])
-    {
-        $arrayData = array_merge(
-            ['code' => $status,],
-            ['message' => $message],
-            ['entity' => $entity->toArray()]
-        );
-
-        parent::__construct($arrayData, $status, $headers);
-    }
 }
---
+++ src/Responses/Response.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Responses;
+
+abstract class Response extends \Symfony\Component\HttpFoundation\Response
+{
+}
