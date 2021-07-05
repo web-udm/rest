b3213781d9fc1f0f329ca5acc7319696ea713fe7
Ildar Mosnabeev <web.udm@gmail.com>
2021-07-05 23:54:49 +0400

    feature-48 Добавил тесты для билдера

--- tests/Unit/Builders/ResponseBuilderTest.php
+++ tests/Unit/Builders/ResponseBuilderTest.php
@@ -3,34 +3,78 @@
 namespace Tests\Unit\Builders;
 
 use App\Builders\ResponseBuilder;
+use App\Entities\EntityInterface;
 use App\Responses\BaseResponse;
+use App\Responses\EntityResponse;
 use PHPUnit\Framework\TestCase;
+use Tests\Unit\Serializers\Helpers\StringHelper;
 
 class ResponseBuilderTest extends TestCase
 {
     public function testBuildJsonBaseResponse()
     {
+        $stringHelper = new StringHelper();
         $responseBuilder = new ResponseBuilder();
         $baseResponse = $responseBuilder->createBaseResponse(404, 'Error');
 
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/base_response.json');
+        $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
+
         $this->assertInstanceOf(BaseResponse::class, $baseResponse);
         $this->assertEquals(404, $baseResponse->getStatusCode());
-        $this->assertEquals(require 'data/base_response.json', $baseResponse->getContent());
+        $this->assertEquals($expectedResponseBodyWithoutSpaces,
+            $stringHelper->cutSpacesAndBreaks($baseResponse->getContent())
+        );
     }
 
     public function testBuildXmlBaseResponse()
     {
+        $stringHelper = new StringHelper();
+        $responseBuilder = new ResponseBuilder();
+        $baseResponse = $responseBuilder->createBaseResponse(404, 'Error', 'xml');
 
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/base_response.xml');
+        $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
+
+        $this->assertInstanceOf(BaseResponse::class, $baseResponse);
+        $this->assertEquals(404, $baseResponse->getStatusCode());
+        $this->assertEquals($expectedResponseBodyWithoutSpaces,
+            $stringHelper->cutSpacesAndBreaks($baseResponse->getContent())
+        );
     }
 
     public function testBuildJsonEntityResponse()
     {
+        $stringHelper = new StringHelper();
+        $responseBuilder = new ResponseBuilder();
+        $entity = $this->createEntity();
+        $entityResponse = $responseBuilder->createEntityResponse($entity);
 
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/entity_response.json');
+        $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
+
+        $this->assertInstanceOf(EntityResponse::class, $entityResponse);
+        $this->assertEquals(200, $entityResponse->getStatusCode());
+        $this->assertEquals($expectedResponseBodyWithoutSpaces,
+            $stringHelper->cutSpacesAndBreaks($entityResponse->getContent())
+        );;
     }
 
     public function testBuildXmlEntityResponse()
     {
+        $stringHelper = new StringHelper();
+        $responseBuilder = new ResponseBuilder();
+        $entity = $this->createEntity();
+        $entityResponse = $responseBuilder->createEntityResponse($entity);
 
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/entity_response.json');
+        $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
+
+        $this->assertInstanceOf(EntityResponse::class, $entityResponse);
+        $this->assertEquals(200, $entityResponse->getStatusCode());
+        $this->assertEquals($expectedResponseBodyWithoutSpaces,
+            $stringHelper->cutSpacesAndBreaks($entityResponse->getContent())
+        );;
     }
 
     public function testBuildJsonEntitiesResponse()
@@ -41,5 +85,21 @@ class ResponseBuilderTest extends TestCase
     public function testBuildXmlEntitiesResponse()
     {
 
+    }
+
+    private function createEntity(): EntityInterface
+    {
+        return new class implements EntityInterface {
+            private $firstname = 'Artem';
+            private $surname = 'Ivanov';
+
+            public function toArray(): array
+            {
+                return [
+                    'firstname' => $this->firstname,
+                    'surname' => $this->surname
+                ];
+            }
+        };
     }
 }
