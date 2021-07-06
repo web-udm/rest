e22a0e4518bd99a3e5f520a6217152a769eefa20
Ildar Mosnabeev <web.udm@gmail.com>
2021-07-06 22:27:39 +0400

    feature-48 Дописал тесты

--- tests/Unit/Builders/ResponseBuilderTest.php
+++ tests/Unit/Builders/ResponseBuilderTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Unit\Builders;
 
 use App\Builders\ResponseBuilder;
+use App\Entities\EntitiesCollection;
 use App\Entities\EntityInterface;
 use App\Responses\BaseResponse;
+use App\Responses\EntitiesResponse;
 use App\Responses\EntityResponse;
 use PHPUnit\Framework\TestCase;
 use Tests\Unit\Serializers\Helpers\StringHelper;
@@ -65,9 +67,9 @@ class ResponseBuilderTest extends TestCase
         $stringHelper = new StringHelper();
         $responseBuilder = new ResponseBuilder();
         $entity = $this->createEntity();
-        $entityResponse = $responseBuilder->createEntityResponse($entity);
+        $entityResponse = $responseBuilder->createEntityResponse($entity, 'xml');
 
-        $expectedResponseBody = file_get_contents(__DIR__ . '/data/entity_response.json');
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/entity_response.xml');
         $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
 
         $this->assertInstanceOf(EntityResponse::class, $entityResponse);
@@ -79,15 +81,49 @@ class ResponseBuilderTest extends TestCase
 
     public function testBuildJsonEntitiesResponse()
     {
+        $stringHelper = new StringHelper();
+        $responseBuilder = new ResponseBuilder();
+        $entitiesCollection = $this->createEntityCollection();
+        $entitiesResponse = $responseBuilder->createEntitiesResponse($entitiesCollection, [
+            "page" => 1,
+            "page_total_count" => 2,
+            "entity_limit" => 3,
+            "entity_total_count" => 4
+        ]);
 
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/entities_response.json');
+        $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
+
+        $this->assertInstanceOf(EntitiesResponse::class, $entitiesResponse);
+        $this->assertEquals(200, $entitiesResponse->getStatusCode());
+        $this->assertEquals($expectedResponseBodyWithoutSpaces,
+            $stringHelper->cutSpacesAndBreaks($entitiesResponse->getContent())
+        );;
     }
 
     public function testBuildXmlEntitiesResponse()
     {
+        $stringHelper = new StringHelper();
+        $responseBuilder = new ResponseBuilder();
+        $entitiesCollection = $this->createEntityCollection();
+        $entitiesResponse = $responseBuilder->createEntitiesResponse($entitiesCollection, [
+            "page" => 1,
+            "page_total_count" => 2,
+            "entity_limit" => 3,
+            "entity_total_count" => 4
+        ], 'xml');
 
+        $expectedResponseBody = file_get_contents(__DIR__ . '/data/entities_response.xml');
+        $expectedResponseBodyWithoutSpaces = $stringHelper->cutSpacesAndBreaks($expectedResponseBody);
+
+        $this->assertInstanceOf(EntitiesResponse::class, $entitiesResponse);
+        $this->assertEquals(200, $entitiesResponse->getStatusCode());
+        $this->assertEquals($expectedResponseBodyWithoutSpaces,
+            $stringHelper->cutSpacesAndBreaks($entitiesResponse->getContent())
+        );
     }
 
-    private function createEntity(): EntityInterface
+    private function createEntity()
     {
         return new class implements EntityInterface {
             private $firstname = 'Artem';
@@ -99,6 +135,30 @@ class ResponseBuilderTest extends TestCase
                     'firstname' => $this->firstname,
                     'surname' => $this->surname
                 ];
+            }
+        };
+    }
+
+    private function createEntityCollection()
+    {
+        $entity1 = $this->createEntity();
+        $entity2 = $this->createEntity();
+
+        return new class($entity1, $entity2) implements EntitiesCollection {
+            private $entities;
+
+            public function __construct(EntityInterface ...$entities)
+            {
+                $this->entities = $entities;
+            }
+
+            public function add(EntityInterface $entity): void
+            {
+            }
+
+            public function all(): array
+            {
+                return $this->entities;
             }
         };
     }
