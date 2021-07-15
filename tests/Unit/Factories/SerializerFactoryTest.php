45d9ae17b94643a611cbf5529acacce84e0bf0f2
moildar <i.mosnabeev@taxi-master.com>
2021-07-15 19:28:21 +0400

    feature-48 Добавил тест для SerializerFactory

--- tests/Unit/Factories/SerializerFactoryTest.php
+++ tests/Unit/Factories/SerializerFactoryTest.php
@@ -2,9 +2,36 @@
 
 namespace Tests\Unit\Factories;
 
+use App\Exceptions\UnknownSerializerType;
+use App\SerializerFactory\SerializerFactory;
+use App\Serializers\JsonSerializer;
+use App\Serializers\XmlSerializer;
 use PHPUnit\Framework\TestCase;
 
 class SerializerFactoryTest extends TestCase
 {
+    public function testCreateJsonSerializer()
+    {
+        $serializerFactory = new SerializerFactory();
+        $serializer        = $serializerFactory->create('json');
 
+        $this->assertInstanceOf(JsonSerializer::class, $serializer);
+    }
+
+    public function testCreateXmlSerializer()
+    {
+        $serializerFactory = new SerializerFactory();
+        $serializer        = $serializerFactory->create('xml');
+
+        $this->assertInstanceOf(XmlSerializer::class, $serializer);
+    }
+
+    public function testCreateNonExistentSerializer()
+    {
+        $serializerFactory = new SerializerFactory();
+
+        $this->expectException(UnknownSerializerType::class);
+
+        $serializerFactory->create('broken_type');
+    }
 }
